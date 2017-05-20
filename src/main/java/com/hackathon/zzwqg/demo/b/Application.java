d38ccca7be5fb0096c0247ce3204c089d665478e
feifan.wang <feifan.wang@tendcloud.com>
2017-05-20 22:02:37 +0800

    slfjsl

--- src/main/java/com/hackathon/zzwqg/demo/b/Application.java
+++ src/main/java/com/hackathon/zzwqg/demo/b/Application.java
@@ -3,6 +3,7 @@ package com.hackathon.zzwqg.demo.b;
 import com.codahale.metrics.MetricRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Bean;
  * Created by zoltar on 17-5-20.
  */
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 

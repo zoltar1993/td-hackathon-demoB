fad045801f1140402a59edf30d028320e30b601e
feifan.wang <feifan.wang@tendcloud.com>
2017-05-20 21:59:23 +0800

    添加监控

--- src/main/java/com/hackathon/zzwqg/demo/b/Application.java
+++ src/main/java/com/hackathon/zzwqg/demo/b/Application.java
@@ -1,20 +1,27 @@
 package com.hackathon.zzwqg.demo.b;
 
+import com.codahale.metrics.MetricRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 
 /**
  * Created by zoltar on 17-5-20.
  */
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+
+    @Bean
+    public MetricRegistry metricRegistry(){
+        return new MetricRegistry();
     }
 
 }
